de6ff53a816858ebc003714cbaee92cc1c140754
Gabrielasaraiv <gabrielasaraivaaraujo358@gmail.com>
2025-04-03 12:59:57 -0300

    first commit

---
+++ app/Livewire/Admin/Funcionario/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Funcionario;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.funcionario.create');
+    }
+}
