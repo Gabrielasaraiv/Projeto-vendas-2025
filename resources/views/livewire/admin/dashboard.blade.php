e3fff41a7ba328d36264015cd667798f17606669
Gabrielasaraiv <gabrielasaraivaaraujo358@gmail.com>
2025-04-03 13:28:05 -0300

    modified:   resources/views/livewire/admin/dashboard.blade.php

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -2,7 +2,9 @@
    <h1> {{ auth()->user()->email }}</h1>
    <h1> {{ auth()->user()->id }}</h1>
    <h1> {{ auth()->user()->user_type }}</h1>
-   <h1> {{ auth()->user()->email }}</h1>
+   <h1> {{ auth()->user()->admin->nome }}</h1>
+
+   <h4>{{ App\Models\Admin::find(1)->user}}<h4>
 
 
    {{ auth()->user() }}
